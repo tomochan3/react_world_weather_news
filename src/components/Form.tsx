43eea44b87325b9a43df6f030331ded3f9040282
tomochan3 <myoga.tomomitsu@gmail.com>
2021-07-12 02:27:06 +0900

    commit

---
+++ src/components/Form.tsx
@@ -0,0 +1,18 @@
+//Form.tsx
+
+type FormPropsType = {
+    city: string;
+    setCity: React.Dispatch<React.SetStateAction<string>>;
+    getWeather:(e:React.FormEvent<HTMLFormElement>) => void;
+}
+
+const Form: React.FC<FormPropsType> = ({city, setCity, getWeather }) => {
+    return (
+        <form onSubmit={getWeather}>
+            <input type="text" name="city" placeholder="都市名(英語で入力)" onChange={e => setCity(e.target.value)} value={city}/>
+            <button type="submit">Get Weather</button>
+        </form>
+    );
+};
+
+export default Form;
